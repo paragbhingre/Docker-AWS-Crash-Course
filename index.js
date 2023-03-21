60ba1a6a92ba3a4bb1d9be81d3314047d140ea9c
Parag Bhingre <pbhingre@amazon.com>
2023-03-21 10:30:07 -0700

    change index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 let app1 = express();
 let app2 = express();
+let app3 = express();
 
 app1.get("/", (req, res) => {
   res.send("This is my express app");
@@ -11,12 +12,20 @@ app1.get("/me", (req, res) => {
   res.send("Hi I am Laith");
 });
 
-app2.get("/", (req, res) => {
+app2.get("/admin", (req, res) => {
   res.send("This is my express app2");
 });
 
-app2.get("/me", (req, res) => {
+app2.get("/admin/me", (req, res) => {
   res.send("Hi I am Laith2");
+});
+
+app3.get("/customer", (req, res) => {
+  res.send("This is csutomer app");
+});
+
+app3.get("/customer/me", (req, res) => {
+  res.send("Hi I am customer");
 });
 
 app1.listen(3000, () => {
@@ -26,3 +35,8 @@ app1.listen(3000, () => {
 app2.listen(3002, () => {
   console.log("Started server on 3002");   
 });
+
+app3.listen(3003, () => {
+  console.log("Started server on 3003");
+});
+
